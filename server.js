0d92b55235478c6b763bc2e2e2a625a44b84b137
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-10 12:07:56 -0800

    Completed PUT and DELETE endpoints.

--- server.js
+++ server.js
@@ -44,4 +44,40 @@ server.post('/api/accounts', (req, res) => {
   })
 })
 
+server.put('/api/accounts/:id', (req, res) => {
+  const accountData = req.body
+  console.log(accountData)
+  const { id } = req.params
+
+  db('accounts').where({ id: id})
+  .update(accountData)
+  .then(account => {
+      if (account) {
+          res.status(200).json(account);
+      } else {
+          res.status(404).json({ message: 'Could not find account with given id.' });
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ message: 'Failed to update account'});
+  })
+})
+
+server.delete('/api/accounts/:id', (req, res) => {
+  const { id } = req.params
+  db('accounts').where({ id: id}).del()
+  .then((count) => {
+      console.log(count)
+      if (count) {
+          console.log(count)
+          res.status(204).end();
+      } else {
+          res.status(404).json({ message: 'Could not find account with given id.' });
+      }
+  })
+  .catch(err => {
+      res.status(500).json({ message: 'Failed to delete account'});
+  })
+})
+
 module.exports = server;
